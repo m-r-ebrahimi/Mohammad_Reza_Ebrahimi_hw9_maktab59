2116ca92c2481f0ed112bbc4c2913386b24e8062
MohammadReza8080 <emohammadreza568@gmil.com>
2021-10-02 10:06:53 +0330

    makePair is paired numbers.

--- Q2/src/ir/maktab/Main.java
+++ Q2/src/ir/maktab/Main.java
@@ -1,6 +1,7 @@
 package ir.maktab;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Main {
@@ -8,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
         fill(numbers);
-
+        System.out.println(find(numbers));
     }
 
     public static void fill(ArrayList<Integer> numbers) {
@@ -27,10 +28,33 @@ public class Main {
         numbers.add(3);
         numbers.add(1);
     }
+
     public static void fill(int i, ArrayList<Integer> numbers) {
         Random rand = new Random();
         for (int j = 0; j < i; j++) {
             numbers.add(rand.nextInt(10));
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> find(ArrayList<Integer> numbers) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (numbers.size() % 2 == 0)
+            makePair(numbers, result, numbers.size());
+        else
+            makePair(numbers, result, numbers.size() - 1);
+        return result;
+    }
+
+    public static void makePair(ArrayList<Integer> numbers, ArrayList<ArrayList<Integer>> result, int size) {
+        for (int i = 0; i < size / 2; i++) {
+            result.add(new ArrayList<>());
+        }
+
+        Iterator<Integer> iterator = numbers.iterator();
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                result.get(i).add(iterator.next());
+            }
         }
     }
 }
