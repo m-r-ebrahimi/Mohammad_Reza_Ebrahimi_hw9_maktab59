e7511d2fe14d24933d769aa8f52759f60c2f6101
MohammadReza8080 <emohammadreza568@gmil.com>
2021-10-05 17:06:54 +0330

    refactor class

---
+++ Q3/src/ir/maktab/LinkedList.java
@@ -0,0 +1,102 @@
+package ir.maktab;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    public class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+
+        size++;
+    }
+
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public void removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+
+        size--;
+    }
+
+    public void removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+
+        size--;
+    }
+
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+}
--- Q4/src/ir/maktab/Main.java
+++ Q4/src/ir/maktab/Main.java
@@ -1,9 +1,50 @@
 package ir.maktab;
 
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
+        HashMap<Character, Integer> map = new HashMap<>();
 
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        char[] c = str.toCharArray();
+        int i = 0;
+        while (i < c.length) {
+            if (map.containsKey(c[i])) {
+                map.put(c[i], map.get(c[i]) + 1);
+            } else {
+                map.put(c[i], 1);
+            }
+            i++;
+        }
+       permute(map);
+
+    }
+    public static void permute(HashMap<Character, Integer> map) {
+        if (map.isEmpty()) {
+            System.out.println();
+        } else {
+            Set<Character> keys = map.keySet();
+
+            for (Character c : keys) {
+
+                System.out.print(c + ",");
+
+                int val = map.get(c) - 1;
+
+                if (val == 0) {
+                    map.remove(c);
+                } else {
+                    map.put(c, val);
+
+                }
+                permute(map);
+            }
+        }
     }
 
 
